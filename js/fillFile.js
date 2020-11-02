7b28caed92f7a639247b6d432504519d600dacbf
Andrew Kapustin <7280567-andrew_kapustin@users.noreply.gitlab.com>
2020-11-02 03:16:44 +0300

    New version of NodeJs

---
+++ js/fillFile.js
@@ -0,0 +1,25 @@
+var documentStorage = require('./Storage');
+
+var run = function () {
+    let id = 0;
+    class SubjectDocument{
+        constructor(name, subject, year, mark){
+            this.name = name;
+            this.id = id++;
+            this.subject = subject;
+            this.year = year;
+            this.mark = mark;
+        }
+    }
+
+    function createDocument(name, subject, year, mark) {
+        documentStorage.addItems(new SubjectDocument(name, subject, year, mark));
+    }
+
+    createDocument('logorifm', 'algebra', 2020, 5);
+    createDocument('english', 'english in mass media', 2018, 6);
+    createDocument('themes', 'deutsch', 2015, 3);
+    createDocument('spain history', 'spanish', 2021, 2);
+}
+
+module.exports = run;
